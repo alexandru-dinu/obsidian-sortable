c2e73e02f1b55240d737bb41d0b9db68b46676ea
alexandru-dinu <alex.dinu07@gmail.com>
2021-04-13 11:17:43 +0300

    Reformat with 4 spaces.

--- main.ts
+++ main.ts
@@ -1,112 +1,113 @@
 import {
-	App, Plugin, PluginSettingTab, Setting
+    App, Plugin, PluginSettingTab, Setting
 } from 'obsidian';
 
 interface SortableSettings {
-	mySetting: string;
+    mySetting: string;
 }
 
 const DEFAULT_SETTINGS: SortableSettings = {
-	mySetting: 'default'
+    mySetting: 'default'
 }
 
 // TODO
-// - (?) arrows should be displayed only on "sortable" tables => "this.asc"
-// - (!) want the event to happen only when clicking on arrows
+// (?) arrows should be displayed only on "sortable" tables => "this.asc"
+// (!) want the event to happen only when clicking on arrows
+// change css for sorting direction
 // https://javascript.plainenglish.io/easy-table-sorting-with-javascript-370d8d97cad8
 // https://stackoverflow.com/questions/14267781/sorting-html-table-with-javascript
 
 export default class SortablePlugin extends Plugin {
-	settings: SortableSettings;
-	asc: boolean = true;
+    settings: SortableSettings;
+    asc: boolean = true;
 
-	compareFn(idx: number, asc: boolean): any {
-		return (a, b) => {
-			const v1 = this.getCellValue(asc ? a : b, idx);
-			const v2 = this.getCellValue(asc ? b : a, idx);
+    compareFn(idx: number, asc: boolean): any {
+        return (a, b) => {
+            const v1 = this.getCellValue(asc ? a : b, idx);
+            const v2 = this.getCellValue(asc ? b : a, idx);
 
-			if (typeof (v1) == 'number' && typeof (v2) == 'number') {
-				return v1 - v2;
-			}
-			else {
-				return v1.toString().localeCompare(v2.toString());
-			}
-		}
-	}
+            if (typeof (v1) === 'number' && typeof (v2) === 'number') {
+                return v1 - v2;
+            }
+            else {
+                return v1.toString().localeCompare(v2.toString());
+            }
+        }
+    }
 
-	tryParseFloat(x: string): string | number {
-		const y = parseFloat(x);
-		return isNaN(y) ? x : y;
-	}
+    tryParseFloat(x: string): string | number {
+        const y = parseFloat(x);
+        return isNaN(y) ? x : y;
+    }
 
-	getCellValue(tr: any, idx: number): string | number {
-		return this.tryParseFloat(tr.children[idx].innerText || tr.children[idx].textContent);
-	}
+    getCellValue(tr: any, idx: number): string | number {
+        return this.tryParseFloat(tr.children[idx].innerText || tr.children[idx].textContent);
+    }
 
-	async onload() {
-		console.log('loading sortable plugin');
+    async onload() {
+        console.log('loading sortable plugin');
 
-		await this.loadSettings();
+        await this.loadSettings();
 
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			const htmlEl = (<HTMLInputElement>evt.target);
+        this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
+            const htmlEl = (<HTMLInputElement>evt.target);
 
-			const table = htmlEl.closest('table');
-			if (table == null) { return; }
+            const table = htmlEl.closest('table');
+            if (table == null) { return; }
 
-			const th = htmlEl.closest('thead th');
-			if (th == null) { return; }
+            const th = htmlEl.closest('thead th');
+            if (th == null) { return; }
 
-			const tbody = table.querySelector('tbody');
-			const thIdx = Array.from(th.parentNode.children).indexOf(th);
+            const tbody = table.querySelector('tbody');
+            const thIdx = Array.from(th.parentNode.children).indexOf(th);
 
-			console.log('Clicked on', th.textContent, thIdx, 'asc:', this.asc);
+            console.log('Clicked on', th.textContent, thIdx, 'asc:', this.asc);
 
-			Array.from(tbody.querySelectorAll('tr:nth-child(n)'))
-				.sort(this.compareFn(thIdx, this.asc))
-				.forEach(tr => tbody.appendChild(tr));
+            Array.from(tbody.querySelectorAll('tr:nth-child(n)'))
+                .sort(this.compareFn(thIdx, this.asc))
+                .forEach(tr => tbody.appendChild(tr));
 
-			this.asc = !this.asc;
-		});
-	}
+            this.asc = !this.asc;
+        });
+    }
 
-	onunload() {
-		console.log('unloading plugin');
-	}
+    onunload() {
+        console.log('unloading plugin');
+    }
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
+    async loadSettings() {
+        this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+    }
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+    async saveSettings() {
+        await this.saveData(this.settings);
+    }
 }
 
 class SortableSettingTab extends PluginSettingTab {
-	plugin: SortablePlugin;
+    plugin: SortablePlugin;
 
-	constructor(app: App, plugin: SortablePlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+    constructor(app: App, plugin: SortablePlugin) {
+        super(app, plugin);
+        this.plugin = plugin;
+    }
 
-	display(): void {
-		let { containerEl } = this;
+    display(): void {
+        let { containerEl } = this;
 
-		containerEl.empty();
+        containerEl.empty();
 
-		containerEl.createEl('h2', { text: 'Sortable Plugin - Settings' });
+        containerEl.createEl('h2', { text: 'Sortable Plugin - Settings' });
 
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Hit me')
-				.setValue('')
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+        new Setting(containerEl)
+            .setName('Setting #1')
+            .setDesc('It\'s a secret')
+            .addText(text => text
+                .setPlaceholder('Hit me')
+                .setValue('')
+                .onChange(async (value) => {
+                    this.plugin.settings.mySetting = value;
+                    await this.plugin.saveSettings();
+                }));
+    }
 }
